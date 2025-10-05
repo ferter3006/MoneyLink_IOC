ee175d1ed0afe725d8ba01cbc581846ffd503304
Ferter <ferratersimon@gmail.com>
2025-10-05 10:41:38 +0200

    sin /api solo /documenation

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -5,14 +5,14 @@ return [
     'documentations' => [
         'default' => [
             'api' => [
-                'title' => 'L5 Swagger UI',
+                'title' => 'Money Link API',
             ],
 
             'routes' => [
                 /*
                  * Route for accessing api documentation interface
                  */
-                'api' => 'api/documentation',
+                'api' => 'documentation',
             ],
             'paths' => [
                 /*
