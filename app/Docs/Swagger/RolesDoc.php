60192474cadb99a7d3e6d4764a5af4ee54d18f82
Ferter <ferratersimon@gmail.com>
2025-10-18 07:45:10 +0200

    petits retocs a docs

--- app/Docs/Swagger/RolesDoc.php
+++ app/Docs/Swagger/RolesDoc.php
@@ -9,11 +9,13 @@ class RolesDoc
 
     /**
      * @OA\Get(
-     *     path="/api/roles",
-     *     operationId="rolesIndex",
+     *     path="/api/roles",     
      *     summary="Devuelve todos los roles. Requiere un token de tipo ADMIN.",
      *     description="Devuelve una lista con todos los roles",
      *     tags={"Roles"},
+     *     security={
+     *         {"bearerAuth"={}}
+     *     },
      *     @OA\Response(
      *         response=200,
      *         description="Operación exitosa",
